08b627510f270bcdccfaf91ddadb410b35f208a4
August R <aggesoft@gmail.com>
2022-05-24 19:58:43 +0200

    cleanup

--- App.tsx
+++ App.tsx
@@ -2,23 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import FlashMessage from "react-native-flash-message";
 
-import useCachedResources from "./hooks/useCachedResources";
-import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
 export default function App() {
-    const isLoadingComplete = useCachedResources();
-    const colorScheme = useColorScheme();
-
-    if (!isLoadingComplete) {
-        return null;
-    } else {
-        return (
-            <SafeAreaProvider>
-                <Navigation colorScheme={colorScheme} />
-                <StatusBar />
-                <FlashMessage position="top" />
-            </SafeAreaProvider>
-        );
-    }
+    return (
+        <SafeAreaProvider>
+            <Navigation />
+            <StatusBar />
+            <FlashMessage position="top" />
+        </SafeAreaProvider>
+    );
 }
