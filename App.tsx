acb78d1b653a4c91057d35742fd2b1211cba342c
August R <aggesoft@gmail.com>
2022-05-25 12:11:37 +0200

    last cleanup?

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
     return (
         <SafeAreaProvider>
             <Navigation />
-            <StatusBar />
+            <StatusBar style="light" />
             <FlashMessage position="top" />
         </SafeAreaProvider>
     );
